3f87f286aebcefb955351804d1132fc97e098627
LuN0oo <antonin.alaphilippe1@gmail.com>
2026-02-25 17:47:20 +0100

    fixed not transparent sprite by drawing floor under them. Also changed the name of the xpm file

--- srcs/ft_file_to_image.c
+++ srcs/ft_file_to_image.c
@@ -6,7 +6,7 @@
 /*   By: analaphi <analaphi@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/02/24 16:08:17 by analaphi          #+#    #+#             */
-/*   Updated: 2026/02/24 16:37:40 by analaphi         ###   ########.fr       */
+/*   Updated: 2026/02/25 17:45:11 by analaphi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,9 +20,9 @@ void	ft_file_to_image(t_map *map)
 	map->img.player = mlx_xpm_file_to_image(map->mlx,
 			"textures/Player.xpm", &a, &a);
 	map->img.strawberry = mlx_xpm_file_to_image(map->mlx,
-			"textures/Collectible.xpm", &a, &a);
+			"textures/Strawberry.xpm", &a, &a);
 	map->img.heart = mlx_xpm_file_to_image(map->mlx,
-			"textures/HeartOpened.xpm", &a, &a);
+			"textures/Exit.xpm", &a, &a);
 	map->img.wall = mlx_xpm_file_to_image(map->mlx,
 			"textures/Wall.xpm", &a, &a);
 	map->img.floor = mlx_xpm_file_to_image(map->mlx,
